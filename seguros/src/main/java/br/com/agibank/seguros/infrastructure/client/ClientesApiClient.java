c2af3583206f8a660f466f488c70c7f63a219952
lukaslopes <lukas.antunes.lopes@gmail.com>
2025-06-24 02:15:20 -0300

    adicionando retry

--- seguros/src/main/java/br/com/agibank/seguros/infrastructure/client/ClientesApiClient.java
+++ seguros/src/main/java/br/com/agibank/seguros/infrastructure/client/ClientesApiClient.java
@@ -7,7 +7,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
+import io.github.resilience4j.retry.annotation.Retry;
 
 @Slf4j
 @Component
@@ -19,6 +21,7 @@ public class ClientesApiClient {
     private final RestTemplate restTemplate;
     private final ClientesApiConfig clientesApiConfig;
     
+    @Retry(name = "clientesApi", fallbackMethod = "fallbackBuscarClientePorCpf")
     public ClienteResponse buscarClientePorCpf(String cpf) {
         try {
             String url = clientesApiConfig.getClientesApiBaseUrl()  + CLIENTES_ENDPOINT;
@@ -34,6 +37,9 @@ public class ClientesApiClient {
         } catch (HttpClientErrorException.NotFound ex) {
             log.info("Cliente não encontrado para o CPF: {}", cpf);
             return null;
+        } catch (ResourceAccessException ex) {
+            log.error("Erro ao buscar cliente por CPF: " + cpf, ex);
+            throw ex;
         } catch (Exception ex) {
             log.error("Erro ao buscar cliente por CPF: " + cpf, ex);
             throw new RuntimeException("Erro ao consultar serviço de clientes", ex);
@@ -42,5 +48,11 @@ public class ClientesApiClient {
     
     public boolean clienteExiste(String cpf) {
         return buscarClientePorCpf(cpf) != null;
+    }
+    
+    // Método de fallback para o retry
+    private ClienteResponse fallbackBuscarClientePorCpf(String cpf, Exception ex) {
+        log.warn("Falha ao buscar cliente após tentativas. CPF: {}", cpf, ex);
+        throw new RuntimeException("Não foi possível consultar o serviço de clientes após várias tentativas", ex);
     }
 }
